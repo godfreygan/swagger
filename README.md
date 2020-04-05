321ded2fe1312d6180a451b0e5449ca138488207
godfrey.gan <g854787652@gmail.com>
2020-04-06 02:12:13 +0800

    add class swagger2markdown

--- README.md
+++ README.md
@@ -15,3 +15,31 @@ order-service_order_manage.yml
 5） 配置站点，指向 swagger/swagger-ui 。具体配置不做赘述。
 
 6） 浏览器访问刚才配置的域名，如：localhost.swagger.com。浏览器默认打开 order-service_order.yml，如果想修改成默认 yml文件，修改 swagger/swagger-ui/index.html 中91行左右的地方，把 'order-service_order.yml' 改为你的想要的默认页，刷新页面即可。
+
+
+
+## swagger2markdown
+
+1)  首先生成json内容的文件，只要是json内容即可。
+
+2） 假设已经配好swagger相关（如上面swagger指示），然后执行生成json内容的文件
+例1：php /swagger/swagger-cli/buildjson.php -f order-service.ini -c order
+例2：php /swagger/swagger-cli/buildjson.php -f order-service.ini -c order -m manage
+
+3） 编写转换文件（或者直接在 md/swagger2md.php 中编写）
+```php
+<?php
+$path = __DIR__;
+require(__DIR__ ."/md/swagger2md.php");
+// 实际参数根据自己情况来配
+$cfg    = ['file_path' => 'swagger.json', 'request_host' => 'http://godfrey.gan.dazhairen.com', 'md_dir_path' => 'docs', 'md_tpl_path' => 'tpl.md', 'is_create_menu' => TRUE, 'menu_file_name' => 'SUMMARY.md'];
+$s2mObj = new Swagger2Md($cfg);
+$s2mObj->transformation();
+?>
+```
+
+4） 在浏览器执行该文件，浏览器没有报错误，且全部成功。OK，恭喜你，已经操作完成了！！！
+
+5） 查看配置的文件夹中是否有生成接口文件、导航文件
+
+**ps：swagger2md是在极短时间内写出来的，可能存在缺陷。如有发现问题，可联系本人（g854787652@gmail.com）修正。 谢谢！！！**
---
+++ swagger-cli/buildjson.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * 执行格式：php build.php -f 模板文件名 -c 项目模块 -m 接口模块
+ * 执行示例：
+ * php build.php -f order-service.ini -c order -m api
+ *
+ */
+require(__DIR__ . "/vendor/autoload.php");
+
+//1个冒号:表此选项需要值但也不是必须，2个冒号::表此选项值可选但也非必须,无冒号则无需传值即不接收值
+$options = getopt('f:c:m:', ["file::", "cloudname::", "modulename"]);
+//var_dump($options);exit;
+$swaggerFileName = getDataVal($options, 'f', getDataVal($options, 'file'));
+if(empty($swaggerFileName)) {
+    $swaggerFileName = 'order-service.ini';
+}
+
+$cloudName = getDataVal($options, 'c', getDataVal($options, 'cloudname'));
+if(empty($cloudName)) {
+    $cloudName = 'order';
+}
+
+$moduleName = getDataVal($options, 'm', getDataVal($options, 'modulename'));
+if(empty($moduleName)) {
+    $moduleName = '';
+}
+
+$iniFile = __DIR__ . '/' . $swaggerFileName;
+if(!file_exists($iniFile)) {
+    exit($iniFile . ' not exists' . PHP_EOL);
+}
+$swaggerIniData = parse_ini_file($iniFile);
+//代码扫描目录
+$scanDir = (isset($swaggerIniData['scandir']) && $swaggerIniData['scandir'])?$swaggerIniData['scandir']:__DIR__ . '/app';
+if(!$scanDir) {
+    exit('scan dir can not empty' . PHP_EOL);
+}
+
+if($swaggerFileName == 'mobile-api.ini'){
+    $realPath = '/app/'. ucfirst($cloudName) .'/';
+} else {
+    $realPath = '/app/'. ucfirst($cloudName) .'/'. ucfirst($moduleName). 'Controllers/';
+}
+
+$scanDir .= $realPath;
+if(!is_dir($scanDir)) {
+    exit($realPath . ' not exists' . PHP_EOL);
+}
+
+$fileName = explode('.', $swaggerFileName);
+$distFile = (isset($fileName[0]) && $fileName[0])?$fileName[0]:'swagger';
+if(empty($moduleName)){
+    $distFile = __DIR__ .'/../swagger-ui/'. $distFile .'_'. $cloudName .'.yml';
+}else{
+    $distFile = __DIR__ .'/../swagger-ui/'. $distFile .'_'. $cloudName . '_' .$moduleName .'.yml';
+}
+echo "scandir:" . $scanDir . PHP_EOL;
+echo "distFile:" . $distFile . PHP_EOL;
+
+$openapi = \OpenApi\scan($scanDir, ['exclude'=>['/app/views', '/app/vendor']]); //第1个参数：扫描要生成文档的代码目录，一般配置问控制器目录
+//第2个参数配置选型，其中exclude配置要忽略的目录
+//header('Content-Type: application/x-yaml');
+$content = $openapi->toJson(); //生成yaml内容
+//echo $content . PHP_EOL;
+$path = dirname($distFile);
+if(!is_dir($path)) {
+    mkdir($path, 0755, true);
+}
+file_put_contents($distFile, $content);
+
+echo 'finish' . PHP_EOL;
+
+function getDataVal($data, $key = null, $default = '') {
+    if(!is_array($data) || is_null($key)) {
+        return $data;
+    }
+    if(!$key) {
+        $key = 0;
+    }
+    if(isset($data[$key])) {
+        return $data[$key];
+    } else {
+        return $default;
+    }
+
+}
